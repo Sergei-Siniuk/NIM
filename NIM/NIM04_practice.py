ab580118796481610b000891c6fb7b4e9892f891
Sergei-Siniuk <102694563+Sergei-Siniuk@users.noreply.github.com>
2022-08-21 16:18:28 +0300

    12345678

--- NIM/NIM04_practice.py
+++ NIM/NIM04_practice.py
@@ -1,17 +1,20 @@
-from nim_engine import put_stones, holder, is_gameover, take_from_bunch,settings,USER_NAME,gena
+from itertools import cycle
+from nim_engine import holder, is_gameover, take_from_bunch, settings, USER_NAME
 from termcolor import cprint, colored
-max_bunche_size,max_bunches,user_count=int(input("Введите максимальное количество камней в кучке: ")),\
-                                       int(input("Введите количество кучек: ")),\
-                                       int(input("Введите количество игровов "))
-settings(max_bunches,max_bunche_size,user_count)
-put_stones()
-user_number = gena(USER_NAME)
-user_color="blue"
-user=(next(user_number))
+
+max_bunche_size, max_bunches, user_count = \
+    int(input("Введите максимальное количество камней в кучке: ")), \
+    int(input("Введите количество кучек: ")), \
+    int(input("Введите количество игровов "))
+settings(max_bunches, max_bunche_size, user_count)
+
+user_number = cycle(USER_NAME.values())
+user_color = "blue"
+user = (next(user_number))
 while True:
-    cprint(f'Текущая позиция \n {holder}', color='green')
-    cprint('Ход игрока {}'.format(user), color=user_color)
-    pos = input(colored('Откуда берем?', color=user_color))
+    cprint(f'Текущая позиция ---{holder}---', color='green')
+    cprint(f'Ход игрока {user}  \n-откуда берем?', color=user_color)
+    pos = input()
     qua = input(colored('Сколько берем?', color=user_color))
     step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
     if step_successed:
@@ -24,4 +27,4 @@ while True:
     if is_gameover():
         break
 
-cprint('Выйграл игрок номер {}'.format(user), color='red')
+cprint(f'Выйграл игрок {user}', color='red')
--- NIM/nim_engine.py
+++ NIM/nim_engine.py
@@ -1,38 +1,17 @@
-from itertools import cycle
 from random import randint
 
 USER_NAME = {}
 holder = []
 
-
-
 def settings(max_bunches, max_bunche_size, count_plaer):
     for i in range(int(count_plaer)):
         USER_NAME[i] = input(f"Введите имя {i + 1} игрока:")
 
-    global MAX_BUNCHES, MAX_BUNCHE_SIZE
-    MAX_BUNCHES = max_bunches
-    MAX_BUNCHE_SIZE = max_bunche_size
-
-def put_stones():
-    """ расположить камни на игровой поверхности """
-    for i in range(1, MAX_BUNCHES + 1):
-        holder.append(randint(1, MAX_BUNCHE_SIZE))
-
-
-
-def gena(a):
-    c = cycle(a.values())
-
-    def generate(c):
-        for i in c:
-            yield i
-
-    return generate(c)
-
-
-
-
+    def put_stones():
+        """ расположить камни на игровой поверхности """
+        for i in range(1, max_bunches + 1):
+            holder.append(randint(1, max_bunche_size))
+    return put_stones()
 
 
 def take_from_bunch(position, quantity):
