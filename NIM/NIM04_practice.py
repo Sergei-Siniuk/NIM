c88c1eecb8f10cf4074e12c2652d74dc3d55f61e
Sergei-Siniuk <102694563+Sergei-Siniuk@users.noreply.github.com>
2022-08-22 08:42:18 +0300

    Class

--- NIM/NIM04_practice.py
+++ NIM/NIM04_practice.py
@@ -1,30 +1,31 @@
 from itertools import cycle
-from nim_engine import holder, is_gameover, take_from_bunch, settings, USER_NAME
+from nim_engine import Nim_bild
 from termcolor import cprint, colored
 
-max_bunche_size, max_bunches, user_count = \
+game = Nim_bild()
+
+game.max_bunche_size, game.max_bunches, game.user_count = \
     int(input("Введите максимальное количество камней в кучке: ")), \
     int(input("Введите количество кучек: ")), \
     int(input("Введите количество игровов "))
-settings(max_bunches, max_bunche_size, user_count)
-
-user_number = cycle(USER_NAME.values())
+game.settings(game.max_bunches, game.max_bunche_size, game.user_count)
+user_number = cycle(game.USER_NAME.values())
 user_color = "blue"
 user = (next(user_number))
 while True:
-    cprint(f'Текущая позиция ---{holder}---', color='green')
+    cprint(f'Текущая позиция ---{game.holder}---', color='green')
     cprint(f'Ход игрока {user}  \n-откуда берем?', color=user_color)
     pos = input()
     qua = input(colored('Сколько берем?', color=user_color))
-    step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
+    step_successed = game.take_from_bunch(position=int(pos), quantity=int(qua))
     if step_successed:
         user_color = 'blue' if user_color == 'yellow' else 'yellow'
         user = (next(user_number))
-        if 0 in holder:
-            holder.remove(0)
+        if 0 in game.holder:
+            game.holder.remove(0)
     else:
         cprint('Невозможный ход!', color='red')
-    if is_gameover():
+    if game.is_gameover():
         break
 
 cprint(f'Выйграл игрок {user}', color='red')
--- NIM/nim_engine.py
+++ NIM/nim_engine.py
@@ -1,26 +1,29 @@
 from random import randint
 
-USER_NAME = {}
-holder = []
 
-def settings(max_bunches, max_bunche_size, count_plaer):
-    for i in range(int(count_plaer)):
-        USER_NAME[i] = input(f"Введите имя {i + 1} игрока:")
+class Nim_bild:
+    def __init__(self):
+        self.USER_NAME = {}
+        self.holder = []
+        self.max_bunches = 0
+        self.max_bunche_size = 0
 
-    def put_stones():
+    def settings(self, max_bunches, max_bunche_size, count_plaer):
+        for i in range(int(count_plaer)):
+            self.USER_NAME[i] = input(f"Введите имя {i + 1} игрока:")
+            self.put_stones()
+
+    def put_stones(self):
         """ расположить камни на игровой поверхности """
-        for i in range(1, max_bunches + 1):
-            holder.append(randint(1, max_bunche_size))
-    return put_stones()
+        for i in range(1, self.max_bunches + 1):
+            self.holder.append(randint(1, self.max_bunche_size))
 
+    def take_from_bunch(self, position, quantity):
+        if self.holder[position - 1] - quantity >= 0:
+            self.holder[position - 1] -= quantity
+            return True
+        else:
+            return False
 
-def take_from_bunch(position, quantity):
-    if holder[position - 1] - quantity >= 0:
-        holder[position - 1] -= quantity
-        return True
-    else:
-        return False
-
-
-def is_gameover():
-    return sum(holder) == 0
+    def is_gameover(self):
+        return sum(self.holder) == 0
